cddac7e8011f421b759001cab02ae0c9d8d871af
gandilong <gandilong@yeah.net>
2013-07-18 17:59:42 +0800

    add activi

---
+++ test/com/thang/schema/Acti.java
@@ -0,0 +1,10 @@
+package com.thang.schema;
+
+import org.activiti.engine.impl.db.DbSchemaCreate;
+
+public class Acti {
+
+	public static void main(String[] args) {
+		DbSchemaCreate.main(null);//创建工作流表
+	}
+}
